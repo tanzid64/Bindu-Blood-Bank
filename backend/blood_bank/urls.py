76d510703d75ccafd4fa789c940f75374847755e
Md Tanzid Haque <tanzid@inbox.ru>
2024-04-19 15:09:49 +0600

    CSRF_TRUSTED_ORIGINS in settings.py -updated

--- backend/blood_bank/urls.py
+++ backend/blood_bank/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include("account.urls")),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+admin.site.index_title = "Bindu"
+admin.site.site_header = "Bindu Admin Panel"
