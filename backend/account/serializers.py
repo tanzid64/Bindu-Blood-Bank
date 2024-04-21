0efb0cd4072c61013e7498b7c406846f67331018
Md Tanzid Haque <tanzid@inbox.ru>
2024-04-22 05:00:04 +0600

    registration serializer -updated

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -8,7 +8,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
         model = User
-        fields = ['username', 'email','gender','blood_group', 'password', 'password2']
+        fields = ['username', 'email','first_name','last_name','gender','blood_group', 'password', 'password2']
         extra_kwargs = {
             'password': {'write_only': True}
         }
